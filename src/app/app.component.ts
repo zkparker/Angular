817bf894bb4284af50ed82705cf2ee1c714f2f47
Zach Parker <zkparker@yahoo.com>
2020-10-16 16:17:59 -0500

    10.16.2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   //title = 'angular-reddit';
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(`Adding article title: ${title.value} and link ${link.value}`);
+    console.log(`Adding article title ${title.value} and link ${link.value}`);
     return false;
   }
 }
