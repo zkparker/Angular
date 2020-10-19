c80bc1e14437ef149462c48f73330947cf6948bd
Zach Parker <zkparker@yahoo.com>
2020-10-19 16:10:20 -0500

    10.19.20 Updates

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,8 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  votes: number;
+  title: string;
+  link: string;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Angular';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+
+  voteUp() {
+    this.votes += 1;
+    return false;
+  }
+
+  voteDown() {
+    this.votes -= 1;
+    return false;
+  }
 
   ngOnInit(): void {
   }
